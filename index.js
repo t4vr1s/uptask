a4d39e99401bf46870d053d5f6d31ad75b65486a
ees <eescobar@computin.cl>
2021-01-07 17:05:09 -0300

    se quito el archivo de variables de entorno y se modifico el host

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(require('./routes/index'));
 
 // servidor y puerto
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
   console.log('escuchando puerto: 3000'.green);
